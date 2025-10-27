1039b32b1ed12d82aeac7a288f5a0de47763f7ea
hekang1992 <hekang19921111@hotmail.com>
2025-10-27 16:27:26 +0800

    first commit

---
+++ leaploan/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  leaploan
+//
+//  Created by hekang on 2025/10/27.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
